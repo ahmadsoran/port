04cbe4133927657a5fd1d9aa982e00d18fc2859f
cydia4kurd <ahmadsorann@gmial.com>
2021-10-08 14:50:19 +0300

    test

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let nvBtn = document.querySelector('.nvBtns');
 let nvBtns = document.querySelectorAll('.nvBtn');
 let navItem = document.querySelector('.nav-item');
 let navItemLink = document.querySelectorAll('.nav-item a');
+let nav = document.querySelector('.navBg');
  
 nvBtn.addEventListener('click',()=>{
 nvBtns.forEach(line => {
     line.classList.toggle('AnimNvBtn');
 });
-navItem.classList.toggle('open');
+
+nav.classList.toggle('show');
+// navItem.classList.toggle('open');
 navItemLink.forEach(link => { 
     link.classList.toggle('fade');
 });
