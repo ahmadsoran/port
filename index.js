0427c16a2d19f9a4c6dd39a9b41cd151b2a4953e
cydia4kurd <ahmadsorann@gmial.com>
2021-10-09 00:00:41 +0300

    fixes

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ let navItemLink = document.querySelectorAll('.nav-item a');
 let nav = document.querySelector('.navBg');
 let cvBTn = document.querySelector('.customBTN1');
  
-nvBtn.addEventListener('click',()=>{
-nvBtns.forEach(line => {
-    line.classList.toggle('AnimNvBtn');
+
+function navMobile (){
+
+    nvBtns.forEach(line => {
+        line.classList.toggle('AnimNvBtn');
+    });
+    
+    navItem.classList.toggle('open');
+    navItemLink.forEach(link => { 
+        link.classList.toggle('fade');
+    });
+    
+}
+
+nvBtn.addEventListener('click', navMobile);
+
+navItemLink.forEach(links => {
+    links.addEventListener('click', navMobile)
 });
 
-navItem.classList.toggle('open');
-navItemLink.forEach(link => { 
-    link.classList.toggle('fade');
-});
-
-} 
-
-
-)
-
-cvBTn.addEventListener('click', ()=>{
-    alert("sorry there is no cv to download ")
-})
