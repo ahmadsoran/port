cfee0f32a7e52320f1111ffb1b8ab133a19ee712
cydia4kurd <ahmadsorann@gmial.com>
2021-10-09 00:24:44 +0300

    link scrool fix

--- index.js
+++ index.js
@@ -21,7 +21,25 @@ function navMobile (){
 
 nvBtn.addEventListener('click', navMobile);
 
+let loopnum= 100;
+
+
+
 navItemLink.forEach(links => {
-    links.addEventListener('click', navMobile)
+    links.addEventListener('click', navMobile);
+   
+  
 });
 
+cvBTn.addEventListener('click', ()=>{
+    alert("sorry this just an example")
+})
+navItemLink.addEventListener('click', function (e) {
+    e.preventDefault();
+  
+    // Matching strategy
+    if (e.target.classList.contains('nav-link')) {
+      const id = e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  }); 
